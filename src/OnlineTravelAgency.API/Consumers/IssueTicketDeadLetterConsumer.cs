6ec9d96aae180f17248266baf93b369a85bbb8fb
armin <a.shoeibi@alibaba.ir>
2022-06-07 15:25:05 +0430

    WIP

---
+++ src/OnlineTravelAgency.API/Consumers/IssueTicketDeadLetterConsumer.cs
@@ -0,0 +1,11 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace OnlineTravelAgency.API.Consumers;
+
+internal class IssueTicketDeadLetterConsumer : AsyncEventingBasicConsumer
+{
+    public IssueTicketDeadLetterConsumer(IModel model) : base(model)
+    {
+    }
+}
---
+++ src/OnlineTravelAgency.API/RabbitMqConsumersBackgroundService.cs
@@ -0,0 +1,39 @@
+﻿using OnlineTravelAgency.API.Consumers;
+using OnlineTravelAgency.Shared.RabbitMq;
+using RabbitMQ.Client;
+
+namespace OnlineTravelAgency.API;
+
+public class RabbitMqConsumersBackgroundService : BackgroundService
+{
+    private readonly ILogger<RabbitMqConsumersBackgroundService> _logger;
+    private readonly IConnection _amqpConnection;
+    private readonly IServiceProvider _serviceProvider;
+
+    public RabbitMqConsumersBackgroundService(ILogger<RabbitMqConsumersBackgroundService> logger, IConnection amqpConnection, IServiceProvider serviceProvider)
+    {
+        _logger = logger;
+        _amqpConnection = amqpConnection;
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        // With DLX
+        await RabbitMqExtensions.WrapRabbitMqCallsWithRetryForEver(() =>
+        {
+            _amqpConnection.CreateConsumer(_serviceProvider, new RabbitMqConsumer<IssueTicketDeadLetterConsumer>
+            {
+                PrefetchCount = 15,
+                GlobalPrefetchCount = true,
+                AutoAcknowledgement = false,
+                BindingDetails = new()
+                {
+                    RoutingKey = "",
+                },
+                QueueDetails = RabbitMqQueues.IssueTicketDeadLetterQueue,
+                ExchangeDetails = RabbitMqExchanges.IssueTicketDeadLetterExchange,
+            });
+        }, stoppingToken, _logger);
+    }
+}
