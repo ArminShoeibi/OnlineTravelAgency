ed21f58be136399152586f5164daa8e5d81b1d45
armin <a.shoeibi@alibaba.ir>
2022-06-07 14:43:12 +0430

    WIP

---
+++ src/OnlineTravelAgency.Shared/RabbitMq/RabbitMqPublisher.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Retry;
+
+namespace OnlineTravelAgency.Shared.RabbitMq;
+public class RabbitMqPublisher
+{
+    private readonly ILogger<RabbitMqPublisher> _logger;
+    private readonly RetryPolicy _retryPolicy;
+    public RabbitMqPublisher(ILogger<RabbitMqPublisher> logger)
+    {
+        _logger = logger;
+        _retryPolicy = Policy.Handle<Exception>()
+                         .WaitAndRetry(5, waitDuration => TimeSpan.FromSeconds(8), onRetry: (exception, waitDuration, retryCount, context) =>
+                         {
+                             _logger.LogError(
+                                 exception,
+                                 "An exception occurred when Publishing Result. WaitDuration: {WaitDuration} - RetryCount: {RetryCount} - Context: {@Context} ",
+                                 waitDuration,
+                                 retryCount,
+                                 context);
+                         });
+    }
+
+
+    public void Publish(byte[] message, string exchangeName, string routingKey, bool publisherConfirms)
+    {
+
+    }
+}
